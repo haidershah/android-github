5cefb993cb6fb8b435ca0326f585e02403a5e716
Rameez Shah <rshah@servicechannel.com>
2019-08-29 15:48:28 -0700

    Initial check-in

---
+++ app/src/main/java/com/example/github/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.github.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.github.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
