1621681db00111484dfee36ceaf86fc5d8a9b5e0
Rameez Shah <rshah@servicechannel.com>
2019-09-10 16:08:26 -0700

    Add up navigation

--- app/src/main/java/com/example/github/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/github/view/activity/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.github.view.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.github.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +11,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.mainNavHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.mainNavHostFragment)
+        return navController.navigateUp()
     }
 }
